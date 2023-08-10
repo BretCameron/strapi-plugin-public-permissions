2b29b22579c486fa4cb53a6ae41ae04eaa0549c5
Bret Cameron <bret@yulife.com>
2023-08-10 16:45:23 +0100

    Remove unused import

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -3,7 +3,6 @@ import { prefixPluginTranslations } from "@strapi/helper-plugin";
 import pluginPkg from "../../package.json";
 import pluginId from "./pluginId";
 import Initializer from "./components/Initializer";
-import PluginIcon from "./components/PluginIcon";
 
 const name = pluginPkg.strapi.name;
 
