a1c41bd586624d0f399e08fcf0e8f84679de2bff
Bret Cameron <bretcameron@gmail.com>
2025-07-14 16:03:18 +0100

    Add auth role

--- server/helpers/db/createPublicRole.ts
+++ server/helpers/db/createPublicRole.ts
@@ -3,10 +3,21 @@ import { Transaction } from "./types";
 
 export const createPublicRole = async (trx: Transaction) => {
   return await trx
-    .insert({
-      name: "Public",
-      description: "Default role given to unauthenticated user.",
-      type: "public",
-    })
+    .insert([
+      {
+        name: "Authenticated",
+        description: "Default role given to authenticated user.",
+        type: "authenticated",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: "Public",
+        description: "Default role given to unauthenticated user.",
+        type: "public",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ])
     .into(TABLE.roles);
 };
